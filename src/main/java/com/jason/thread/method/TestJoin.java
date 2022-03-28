ae9fd32bec1574e2df3b6c98e17d71dc55631bcd
chenghaojason <whx20100101@163.com>
2022-03-28 16:14:26 +0800

    修改项目结构

---
+++ src/main/java/com/jason/thread/method/TestJoin.java
@@ -0,0 +1,13 @@
+package com.jason.thread.method;
+
+/**
+ * 测试线程中的join()方法
+ *
+ * @author WongChenHoll
+ * @date 2022-3-25 17:00
+ **/
+public class TestJoin {
+    public static void main(String[] args) {
+
+    }
+}
